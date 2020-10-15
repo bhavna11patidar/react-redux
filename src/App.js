b67f2e559ef62dcb19a382cd7bb11cd0f385bc54
unknown <bhavna11patidar@gmail.com>
2020-10-15 15:18:40 +0530

    redux 1 lecture

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {createStore} from 'redux';
+/*Flow
+1. Store:- to maintain the state globally
+2. Action:- Add, edit, increment, decrement
+3. Reducer:- to manupulate the state according to the action
+4. dispatch:-  
+*/
+
+const increment=()=>{
+    return {
+      type:"INCREMENT",
+    }
+}
+
+const decrement=()=>{
+  return {
+    type:"DECREMENT",
+  }
+}
+
+const reducer=(state=0, action)=>{
+    switch(action.type){
+      case "INCREMENT":
+        return state+1;
+      case "DECREMENT":
+          return state-1;
+      default:
+        return state
+    }
+}
+
+const store=createStore(reducer);
+store.subscribe(()=>{console.log(store.getState())});
+
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(decrement());
+store.dispatch(decrement());
+store.dispatch(decrement());
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Hello World
+     </h1>
     </div>
   );
 }
