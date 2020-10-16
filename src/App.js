6f879d54b592aff44da2a926c75d1373802f9c9e
unknown <bhavna11patidar@gmail.com>
2020-10-16 15:46:58 +0530

    redux using separate files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
-import {createStore} from 'redux';
+import {increment, decrement, isLoggedInUser} from './actions';
+import {useSelector, useDispatch} from 'react-redux'; 
+//import {createStore} from 'redux';
 /*Flow
 1. Store:- to maintain the state globally
 2. Action:- Add, edit, increment, decrement
 3. Reducer:- to manupulate the state according to the action
 4. dispatch:-  
-*/
+
 
 const increment=()=>{
     return {
@@ -42,13 +44,24 @@ store.dispatch(increment());
 store.dispatch(decrement());
 store.dispatch(decrement());
 store.dispatch(decrement());
+*/
+
+
+
 
 
 function App() {
+  const counter=useSelector(state=>state.counter)
+  const isLogged=useSelector(state=>state.isLogged)
+  const dispatch=useDispatch();
   return (
     <div className="App">
-     <h1>Hello World
-     </h1>
+     <h1>Hello World</h1>
+      <h1>Counter {counter}</h1>
+      <button onClick={()=>dispatch(increment())}>+</button><br></br>
+      <button onClick={()=>dispatch(decrement())}>-</button><br></br>
+      <button onClick={()=>dispatch(isLoggedInUser())}>Log In</button><br></br>
+      {isLogged?<h1>You r logged In</h1>:<h1>Please Login</h1>}
     </div>
   );
 }
